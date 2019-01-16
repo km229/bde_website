fe72d5293a1b131bac676cef59b0d7c44f023e58
Mmouky <33157839+Mmouky@users.noreply.github.com>
2019-01-16 17:05:49 +0100

    bootstrap + jquery
    bootstrap + jquery

--- site_bde/resources/views/template.blade.php
+++ site_bde/resources/views/template.blade.php
@@ -3,13 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @yield('title')
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-        <link href="public/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        @yield('title')
     </head>
     <body>
         @yield('body')
+        <script src="{{ asset('js/bootstrap.min.js') }}">
+        <script src="{{ asset('js/jquery-3.3.1.min.js') }}">
     </body>
-    <script src="site_bde/public/js/bootstrap.min.js"/>
-    <script src="site_bde/public/js/jquery-3.3.1.min.js"/>
 </html>
