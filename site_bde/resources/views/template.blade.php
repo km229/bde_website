086f095c7830053c8652e4134f47df8d70de7963
Mmouky <33157839+Mmouky@users.noreply.github.com>
2019-01-16 19:37:43 +0100

    MAJ (encore)
    maj footer + header + ajout style.css en attendant d'avoir mieux

--- site_bde/resources/views/template.blade.php
+++ site_bde/resources/views/template.blade.php
@@ -3,15 +3,20 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
         @yield('title')
     </head>
     <body>
         <header>
-            <h1>Site BDE</h1>
+            <h1>Super titre :)</h1>
         </header>
         @yield('body')
+        <footer>
+            <p>
+                Super footer :)
+            </p>
+        </footer>
         <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
         <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
     </body>
