fa1747f633dad4ada32d52b250f91d0b95efc9d0
Mmouky <33157839+Mmouky@users.noreply.github.com>
2019-01-16 14:58:15 +0100

    Template
    template + yield

---
+++ site_bde/resources/views/template.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @yield('title')
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+        @yield('body')
+    </body>
+</html>
