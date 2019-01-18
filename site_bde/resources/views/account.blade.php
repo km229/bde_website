ec0dcf48410ac1f1bd0121080b113fd58a518d8e
Peliagos <moaw200@gmail.com>
2019-01-18 16:45:14 +0100

    Account

--- site_bde/resources/views/account.blade.php
+++ site_bde/resources/views/account.blade.php
@@ -5,6 +5,18 @@ Ideas
 @endsection
 
 @section('body')
-	Account
+wtf
+<div class="row">
+    <div class="col-lg-3">
+        <h1 class="my-4">My account</h1>
+        <a href="/account/orders" class="list-group-item black">My orders</a>
+        <a href="/account" class="list-group-item black">My informations</a>
+    </div>
+	<div class="col-lg-2">
+		<p>Firstname :</p>
+		<p>Lastname :</p>
+		<p>Email :</p>
+		<p>Location :</p>
+	</div>
 @endsection
 
--- site_bde/resources/views/orders.blade.php
+++ site_bde/resources/views/orders.blade.php
@@ -5,6 +5,13 @@ My orders
 @endsection
 
 @section('body')
-	Orders
+<div class="row">
+    <div class="col-lg-3">
+        <h1 class="my-4">My orders</h1>
+            <a href="/account/orders" class="list-group-item black">My orders</a>
+            <a href="/account" class="list-group-item black">My informations</a>
+        </div>
+    </div>
+</div>
 @endsection
 
