37c8e1b3f2347dbf1ebb36b777d6d9f08aed725f
km229 <kevin.moriceau@viacesi.fr>
2019-01-18 12:51:54 +0100

    register

--- site_bde/resources/views/register.blade.php
+++ site_bde/resources/views/register.blade.php
@@ -6,7 +6,11 @@ Sign up
 
 @section('body')
 <h1>Register</h1>
-<p>If you already have an account, <a href="login">please login here</a></p>";
+
+<p>If you already have an account, <a href="login">please login here</a></p>"
+
+
+<p>The password must:</p>"
 
 <div class="container">
 	{!! form($form) !!}
