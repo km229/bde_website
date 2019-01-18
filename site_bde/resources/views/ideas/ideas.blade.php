907f9a70ce24a55d4bb31001d6b52f817ce4d455
Peliagos <moaw200@gmail.com>
2019-01-18 13:02:30 +0100

    Change

--- site_bde/resources/views/ideas/ideas.blade.php
+++ site_bde/resources/views/ideas/ideas.blade.php
@@ -5,6 +5,13 @@ Ideas
 @endsection
 
 @section('body')
-	Ideas
+	<h2 id="test">Suggestion box</h2>
+	<article>	
+		<p>Suggestion box allows students of the school to give event ideas to BDE members.</p>
+	</article>
+	<style>
+    	.dotted {border: 5px dotted #fff; border-style: none none dotted; }
+	</style>	
+	<hr class='dotted' />
 @endsection
 
