63d2946a7d2e490327d1d1844031b237dfb1938f
Peliagos <moaw200@gmail.com>
2019-01-18 14:00:48 +0100

    logo+

--- site_bde/resources/views/ideas/ideas.blade.php
+++ site_bde/resources/views/ideas/ideas.blade.php
@@ -13,5 +13,41 @@ Ideas
     	.dotted {border: 5px dotted #fff; border-style: none none dotted; }
 	</style>	
 	<hr class='dotted' />
+<div class="row">
+
+    <div class="col-lg-3">
+
+        <h1 class="my-4">BDE CESI Saint-Nazaire</h1>
+        <div class="card my-4">
+            <h5 class="card-header">Search</h5>
+            <div class="card-body">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for...">
+                    <span class="input-group-btn">
+                  <button class="btn btn-secondary" type="button">Go!</button>
+                </span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.col-lg-3 -->
+
+    <div class="col-lg-9">
+
+        <div class="row">
+    @foreach ($ideas as $idea)
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+                <div class="card-body card-body2">
+                    <h4 class="card-title">
+                        <a href="#"><?php  echo $idea -> idea_title ?></a>
+                    </h4>
+                    <p><?php  echo $idea -> idea_desc ?></p>
+                </div>
+            </div>
+        </div>
+    @endforeach
+        </div>
+    </div>
 @endsection
 
