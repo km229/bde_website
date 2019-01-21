88150c825b9375fc3ef7c8f23205e20efccb75e8
km229 <kevin.moriceau@viacesi.fr>
2019-01-21 12:33:22 +0100

    img resolved (2)

--- site_bde/resources/views/welcome.blade.php
+++ site_bde/resources/views/welcome.blade.php
@@ -77,7 +77,7 @@ Site
 				<h3>'. $products[$i] -> product_name .'</h3>
 				<img class="d-block w-100" src="';
 				if(isset($products[$i] -> product_img)){
-					echo 'data:image/jpeg;base64,'. base64($products[$i] -> product_img) .'" >';
+					echo 'data:image/jpeg;base64,'. base64_encode($products[$i] -> product_img) .'" >';
 				} else { echo asset('img/noimg.jpg').'" >'; }
 				echo '<a href="products/' . $products[$i] -> product_id . '" ></a></div>';
 			}
