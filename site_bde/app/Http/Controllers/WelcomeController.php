7a811902a788058c965f08c640ec43156eb2848e
km229 <kevin.moriceau@viacesi.fr>
2019-01-21 10:35:39 +0100

    Recent activites & best products

--- site_bde/app/Http/Controllers/WelcomeController.php
+++ site_bde/app/Http/Controllers/WelcomeController.php
@@ -8,7 +8,8 @@ use Illuminate\Support\Facades\DB;
 class WelcomeController extends Controller
 {
     public function index(){
-        $activities = DB::table('activity')->orderBy('activity.activity_date', 'DESC')->get();
-        return view('welcome', compact("activities"));
+        $activities = DB::table('activity')->orderBy('activity.activity_date', 'DESC')->limit(4)->get();
+        $products = DB::table('product')->orderBy('product.product_sales_number', 'DESC')->limit(4)->get();
+        return view('welcome', compact("activities"), compact("products"));
     }
 }
